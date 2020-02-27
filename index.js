494017af770e04da4cefcbd02545208ee1463411
Christina Harris <christinathedesigner@outlook.com>
2020-02-27 08:41:11 -0800

    second exercise complete

--- index.js
+++ index.js
@@ -79,28 +79,24 @@ class Person {
 class Car {
   constructor(model, milesPerGallon) {
     this.model = model;
-    this.milesPerGallon = 
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    if (this.tank - distance / this.milesPerGallon <= 0) { 
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+     return `I ran out of fuel at ${this.odometer} miles!`
+    }
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
   }
 }
 
-function Car(model, milesPerGallon) {
-  this.model = model;
-  this.milesPerGallon = milesPerGallon;
-  this.tank = 0;
-  this.odometer = 0;
-}
- Car.prototype.fill = function(gallons) {
-  this.tank += gallons;
- }
- Car.prototype.drive = function(distance) {
-   if (this.tank - distance / this.milesPerGallon <= 0) { 
-     this.odometer += this.tank * this.milesPerGallon;
-     this.tank = 0;
-    return `I ran out of fuel at ${this.odometer} miles!`
-   }
-   this.odometer += distance;
-   this.tank -= distance / this.milesPerGallon;
- }
 
 /*
   TASK 3
